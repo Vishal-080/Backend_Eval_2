e5c650709a8f9d41a979712ee181f713a110ea29
Vishal Rathod <vrathod591@gmail.com>
2021-10-18 16:13:09 +0530

    register and login

--- src/controllers/userAuth.controllers.js
+++ src/controllers/userAuth.controllers.js
@@ -1,11 +1,33 @@
 const User = require ("./models/user.models");
+const jwt = require("jsonwebtoken");
+const newToken = (user) => {
+    return jwt.sign((user),process.env.JWT_SECRET_KEY);
+}
 
 const register = async (req, res) => {
     let user ;
 
     user = await User.findOne({name: req.body.name});
+    user = await User.findOne({email: req.body.email});
+    if (user) return res.status(400).send({message:"Enter Correct email and password"});
+    user = await User.create(req.body);
+    const token = newToken(user);
+    return res.status(200).send({token: token});
+
 }
 
 const login = async (req, res) => {
+    let user ;
+
+    user = await User.findOne({name: req.body.name});
+    user = await User.findOne({email: req.body.email});
+    if ( ! user) return res.status(400).send({message:"Enter Correct email and password"});
+    let match = User.checkPassword(req.body.password);
+    if (!match) return res.status(400).send({message:"Invalid password"});
+    const token = newToken(user);
+    return res.status(200).send({token: token});
 
 }
+
+
+module.exports = {register,login};
